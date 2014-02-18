b4e7e21c150ed452eb8c14b8adf355957a00331c
venkateshthallam <venkateshthallam@gmail.com>
2014-02-18 02:32:10 -0500

    First push of all code

---
+++ SendSMSUsingNexmo.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+	include ( "NexmoMessage.php" );
+
+
+	/**
+	 * To send a text message.
+	 *
+	 */
+
+	// Step 1: Declare new NexmoMessage.
+	$nexmo_sms = new NexmoMessage('67f06d4b', 'c2898b24');
+
+	// Step 2: Use sendText( $to, $from, $message ) method to send a message. 
+	$info = $nexmo_sms->sendText( '+13474808495', '+15022193799', 'Hey Buddy,like every day i snoozed and slept off today,please call me or come kick my ass, but wake me up :D !' );
+
+	// Step 3: Display an overview of the message
+	echo $nexmo_sms->displayOverview($info);
+
+	// Done!
+
+?>
